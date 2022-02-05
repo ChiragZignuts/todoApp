981449a8fc2bfe3d8813698317b0f3ce245fc5d2
ChiragZignuts <chiragt@zignuts.com>
2022-02-05 18:47:05 +0530

    added database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,27 +1,46 @@
 var bodyParser = require('body-parser');
+const read = require('body-parser/lib/read');
 var mongoose = require('mongoose');
 
 // Connect to the database
-mongoose.connect('mongodb+srv://test:test@todoapp.cx5os.mongodb.net/todo?retryWrites=true&w=majority')
+var dbURI = 'mongodb+srv://test:test@todoapp.cx5os.mongodb.net/todoApp?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true}).then((result) => console.log('connected to db')).catch((err) => console.log(err));
 
-var data = [{item: 'Get Milk'}, {item: 'Walk Dog'}, {item: 'Do Coding'}]
+// Create a schema
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+//var data = [{item: 'Get Milk'}, {item: 'Walk Dog'}, {item: 'Do Coding'}]
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
 
     app.get('/todo', function(req, res){
-        res.render('todo', {todos : data});
+        // get data from mongodb and pass it to view
+        Todo.find({}, function(err, data){
+            if (err) throw err;
+            res.render('todo', {todos : data});
+        });
     });
 
     app.post('/todo', urlencodedParser, function(req, res){
+        // get data from view and add to mongodb
+        var newTodo = Todo(req.body).save(function(err, data){
+            if (err) throw err;
+            res.json(data);
+        });
         data.push(req.body);
         res.json(data);
     });
 
     app.delete('/todo/:item', function(req, res){
-        data = data.filter(function(todo){
-            return todo.item !== req.params.item;
+        // delete the requested item from mongodb
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+            if (err) throw err;
+            read.json(data);
         });
-        res.json(data);
     });
 }
